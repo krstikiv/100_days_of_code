390487be37058491ee4e01a06353dbe766cffa39
Iva Krstik <krstikiv@gmail.com>
2017-01-09 18:03:11 +0100

    Improve Autocomplete (Jquery) and User Enter event

--- Wikipedia Search Viewer/wiki.js
+++ Wikipedia Search Viewer/wiki.js
@@ -1,50 +1,56 @@
+//Run some jQuery
 $(document).ready(function() {
-  $('#search').click(function() {
-
-    var userSearch = $(".input-field").val();
-    var url = 'http://en.wikipedia.org/w/api.php';
-
+  
+ //Create a click function when user clicks Search 
+  $("#btnsearch").click(function() {
+    //Get user's input
+    var searchTerm = $('#userInput').val();
+    //Edit wiki's api to include the search term
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+    
+    console.log(url); //Check link
+    console.log(searchTerm); //Check if user's input is captured
     $.ajax({
+      type: "GET",
       url: url,
-      data: {
-        action: 'opensearch',
-        search: userSearch,
-        limit: 10,
-        format: 'json'
-      },
-      dataType: 'jsonp',
-      success: processResult
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function(data) {
+        $('#display-result').html('');
+        //display Wiki's data into the div element. 
+        for (var i = 0; i < data[1].length; i++) {
+          $('#display-result').prepend("<div><div><a href=" + data[3][i] + "><h2>" + data[1][i] + "</h2></a>" + "<p>" + data[2][i] + "</p></div></div>");
+        }
 
+        $("#userInput").val(''); //clear the search field
+             },
+      error: function(errorMessage) {
+        console.log(errorMessage);
+      }
+      
     });
+ return false; 
   });
-});
 
-//autocomplete function
-$(".input-field").autocomplete({
-    autoFocus: true,
+  //autocomplete function
+var url = "https://en.wikipedia.org/w/api.php";
+ $("#userInput").autocomplete({
+   autoFocus: true,
     source: function(request, response) {
-        $.ajax({
-            url: "http://en.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                'action': "opensearch",
-                'format': "json",
-                'search': request.term
-            },
-            success: function(data) {
-                response(data[1]);
-            },
-            error: function () {
-              response([]);
-            }
-        });
+      $.ajax({
+        url: url,
+        dataType: "jsonp",
+        data: {
+          action: "opensearch",
+          format: "json",
+          search: request.term
+        },
+        success: function(data) {
+          response(data[1]);
+        }
+      });
     }
+  }); 
+
 });
-
-
-
-function processResult(data) {
-  for (var i = 0; i < data[1].length; i++) {
-    $('#display-result').prepend("<div><a href="+data[3][i]+">" + data[1][i] + "</a>" + "<p>" + data[2][i] + "</p></div>");
-  }
-}
